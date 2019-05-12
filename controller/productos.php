4ba49aa61e15bc76d73f1435c254cedb473618c0
Julián Melero <julian@melerohidalgo.es>
2019-05-12 19:45:09 +0200

    Sólo se puede acceder a los productos si es admin

--- controller/productos.php
+++ controller/productos.php
@@ -2,10 +2,16 @@
 require_once getcwd()."/model/productos.php";
 class productos {   
     function ver(){
-    // Llamamos a la página principal
-    $productos = new model_productos("ecobabyplanet");
-    $resultado = $productos->get_productos();
-    require_once getcwd()."/view/productos.php"; 
+        session_start();
+    // Comprobamos que pueda acceder
+    if(isset($_SESSION["rol"])){
+        $productos = new model_productos("ecobabyplanet");
+        $resultado = $productos->get_productos();
+        require_once getcwd()."/view/productos.php"; 
+    }
+    else{
+        header( "Location: index.php");
+    }
 
     }
 
