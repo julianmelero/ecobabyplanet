0c1aad74592b9220b8921d96eccecd75d213108c
Elizabeth Pereira <epereiraf@fp.uoc.edu>
2019-05-15 13:43:38 +0000

    Update suscripcion.php

--- model/suscripcion.php
+++ model/suscripcion.php
@@ -1,27 +1,12 @@
 <?php
 include_once "model.php";
 
-class suscripcion {
+class model_suscripcion extends model{
 
-    public $id_suscripcion;
-    public $nombre;
-    public $descripcion;
-    public $imagen;
-    public $precio;
-    public $divisa;
-
-    public function listar_suscripciones($id_suscripcion) {
-        try {
-            $stm = $this->pdo->prepare("SELECT * FROM id_suscripcion = ? ORDER BY id_suscripcion ASC");
-            $stm->execute(array($id_suscripcion));
-            return $stm->fetchALL (PDO::FETCH_OBJ);
-        }
+    public function getLista() {
+        $sql = "SELECT * FROM suscripcion";
+        return $this->query($sql, array());
     }
-
-
-
-    
 }
-
 
 
